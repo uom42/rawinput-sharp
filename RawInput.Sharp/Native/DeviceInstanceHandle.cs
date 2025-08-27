9744429e41047c4a70b2289d676823eaeda55b87
uom <uom.2000@gmail.com>
2025-08-27 14:49:57 +0700

    DeviceInstanceHandle : Vanara.PInvoke.Kernel32.SafeHFILE

--- RawInput.Sharp/Native/DeviceInstanceHandle.cs
+++ RawInput.Sharp/Native/DeviceInstanceHandle.cs
@@ -5,29 +5,39 @@ namespace UOM.WinAPI.Windows.RawInput.Native;
 /// <summary>
 /// DEVINST
 /// </summary>
-public readonly struct DeviceInstanceHandle : IEquatable<DeviceInstanceHandle>
+public class DeviceInstanceHandle : Vanara.PInvoke.Kernel32.SafeHFILE, IEquatable<DeviceInstanceHandle>
 {
-    readonly IntPtr value;
+	readonly IntPtr value;
 
-    public static DeviceInstanceHandle Zero => (DeviceInstanceHandle)IntPtr.Zero;
+	public static DeviceInstanceHandle Zero
+		=> (DeviceInstanceHandle) IntPtr.Zero;
 
-    DeviceInstanceHandle(IntPtr value) => this.value = value;
+	DeviceInstanceHandle ( IntPtr value ) : base (value, true)
+		=> this.value = value;
 
-    public static IntPtr GetRawValue(DeviceInstanceHandle handle) => handle.value;
+	public static IntPtr GetRawValue ( DeviceInstanceHandle handle )
+		=> handle.value;
 
-    public static explicit operator DeviceInstanceHandle(IntPtr value) => new(value);
+	public static explicit operator DeviceInstanceHandle ( IntPtr value )
+		=> new (value);
 
-    public static bool operator ==(DeviceInstanceHandle a, DeviceInstanceHandle b) => a.Equals(b);
+	public static bool operator == ( DeviceInstanceHandle a, DeviceInstanceHandle b )
+		=> a.Equals (b);
 
-    public static bool operator !=(DeviceInstanceHandle a, DeviceInstanceHandle b) => !a.Equals(b);
+	public static bool operator != ( DeviceInstanceHandle a, DeviceInstanceHandle b )
+		=> !a.Equals (b);
 
-    public bool Equals(DeviceInstanceHandle other) => value.Equals(other.value);
+	public bool Equals ( DeviceInstanceHandle? other ) => value.Equals (other?.value);
 
-    public override bool Equals(object? obj) =>
-        obj is DeviceInstanceHandle other &&
-        Equals(other);
+	public override bool Equals ( object? obj )
+		=>
+		obj is DeviceInstanceHandle other &&
+		Equals (other);
 
-    public override int GetHashCode() => value.GetHashCode();
+	public override int GetHashCode ()
+		=> value.GetHashCode ();
 
-    public override string ToString() => value.ToString();
+	public override string ToString ()
+		=> value.ToString ();
+
 }
